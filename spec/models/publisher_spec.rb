832472629d73728fb3a8a2899f9dd4254ee545c3
youshengchang <changyousheng@yahoo.com>
2015-12-06 20:13:06 +0000

    add one-to-many relationship

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Publisher, :type => :model do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
-    
+    it { should have_many(:books) }
 end
