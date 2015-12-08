f96c4bad0f50ee2ccd3ff15c47d3fe9a16fc82f0
youshengchang <changyousheng@yahoo.com>
2015-12-08 16:30:49 +0000

    add book feature test

---
+++ spec/features/creating_book_feature_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Creating Books" do
+    
+    let!(:peachpit) { Fabricate(:publisher, name: 'Peachpit Press') }
+    let!(:author1) { Fabricate(:author) }
+    let!(:author2) { Fabricate(:author) }
+    
+    scenario "with valid inputs succeed" do
+        
+        visit root_path
+        
+        click "Books", exact: true
+        click "Add New Book"
+        
+        fill_in "Title", with: "Javascript"
+        fill_in "isbn", with: "9780321772978"
+        fill_in "Page count", with: 518
+        fill_in "Price", with: 34.99
+        fill_in "Description", with: "Learn Javascript with a quick and easy way"
+        fill_in "Published at", with: "2014-01-01"
+        select "Peachpit Press", from: "Publisher"
+        check author1.full_name
+        check author2.full_name
+        
+        
+        click_button "Create Book"
+        
+        expect(page).to have_content("Book has been created")
+    end
+    
+end
